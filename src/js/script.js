6f15857a553e3dfbddcf4a112dd8c70ee2f8ae9f
Vlad <kulus20@gmail.com>
2020-04-26 20:34:59 -0500

    appData.choseIncome(); function added

--- src/js/script.js
+++ src/js/script.js
@@ -1,116 +1,159 @@
-// 'use strict';
-// let money, date;
-// function askingBudget(){
-//     money = prompt("Ваш бюджет на месяц", '');
-//     date  = prompt("Введите сегодняшнюю дату");
-// }
-// askingBudget();
-// let appData = {
-//     budget: money,
-//     expenses: {},
-//     currentDate: date,
-//     optionalExpenses: {},
-//     income : [],
-//     savings : true
-// };
-
-// for(let i = 0; i < 2; i++){
-//     let a = prompt('Введите название основной статьи расходов в этом месяце', ''),
-//         b = +prompt('Введите стоимость', '0');
-//     if( (typeof(a)) === "string" && (typeof(a)) !== null && (typeof(b)) !== null && (a.length) < 50 && !isNaN(b) && (a.length) > 0){
-//         console.log("passed");
-//         appData.expenses[a] = b;
-//     }
-//     else if((a.length) > 50){
-//         i--;
-//         alert("Название статьи расходов не может превишать 50 символов - нажмите ОК и попробуйте еще раз");
-//     }
-//     else if((a.length) == 0){
-//         i--;
-//         alert("Название статьи расходов не может быть пустым. Нажмите ОК и попробуйте снова");
-//     }
-//     else if((typeof(a)) !== "string"){
-//         i--;
-//         alert("Название статьи расходов не может состоять только из чисел");
-//     }
-//     else if((typeof(a)) == null || (typeof(b)) == null){
-//         i--;
-//         alert("Вы не заполнили название обязательной статьи расходов или не ввели стоимость");
-//     }
-//     else if(isNaN(b)){
-//         i--;
-//         alert("В поле стоимость можно ввести только число");
-//     }
-//     else{
-//         console.log('not passed');
-//         alert("Что то пошло не так. Попробуйте перезагрузить страницу и ввести данные заново");
-//     }
-// }
-// function chooseOptExpenses(){
-//     for(let j=1; j < 4; j++){
-
-//         let d = prompt("Статья необязательных расходов", "");
-
-//         if((typeof(d)) === "string" && (typeof(d)) !== null){
-//             appData.optionalExpenses[j] = d;
-//         }
-//         else if(!d){
-//             break;
-//         }
-//         else{
-//             console.log("Функция не сработала");
-//         }
-        
-//     }
-// }
-
-// chooseOptExpenses();
-
-// function detectDayBudget(){
-//     let month = 30;
-//     appData.oneDayBudget = appData.budget/month;
-//     alert(Math.round(appData.oneDayBudget));
-// }
-// detectDayBudget();
-
-// function detectLevel(){
+'use strict';
+let money, date;
+function askingBudget(){
+    money = prompt("Ваш бюджет на месяц", '');
+    date  = prompt("Введите сегодняшнюю дату");
+}
+askingBudget();
+let appData = {
+    budget: money,
+    expenses: {},
+    currentDate: date,
+    optionalExpenses: {},
+    income : [],
+    savings : true,
+    choseExpenses: function(){
+        for(let i = 0; i < 2; i++){
+            let a = prompt('Введите название основной статьи расходов в этом месяце', ''),
+                b = +prompt('Введите стоимость', '0');
+            if( (typeof(a)) === "string" && (typeof(a)) !== null && (typeof(b)) !== null && (a.length) < 50 && !isNaN(b) && (a.length) > 0){
+                console.log("passed");
+                appData.expenses[a] = b;
+            }
+            else if((a.length) > 50){
+                i--;
+                alert("Название статьи расходов не может превишать 50 символов - нажмите ОК и попробуйте еще раз");
+            }
+            else if((a.length) == 0){
+                i--;
+                alert("Название статьи расходов не может быть пустым. Нажмите ОК и попробуйте снова");
+            }
+            else if((typeof(a)) !== "string"){
+                i--;
+                alert("Название статьи расходов не может состоять только из чисел");
+            }
+            else if((typeof(a)) == null || (typeof(b)) == null){
+                i--;
+                alert("Вы не заполнили название обязательной статьи расходов или не ввели стоимость");
+            }
+            else if(isNaN(b)){
+                i--;
+                alert("В поле стоимость можно ввести только число");
+            }
+            else{
+                console.log('not passed');
+                alert("Что то пошло не так. Попробуйте перезагрузить страницу и ввести данные заново");
+            }
+        }
+    },
+    chooseOptExpenses: function (){
+        for(let j=1; j < 4; j++){
+    
+            let d = prompt("Статья необязательных расходов", "");
+    
+            if((typeof(d)) === "string" && (typeof(d)) !== null){
+                appData.optionalExpenses[j] = d;
+            }
+            else if(!d){
+                break;
+            }
+            else{
+                console.log("Функция не сработала");
+            }
+            
+        }
+    },
+    detectDayBudget: function (){
+        let month = 30;
+        appData.oneDayBudget = appData.budget/month;
+        alert(Math.round(appData.oneDayBudget));
+    },
+    detectLevel: function (){
     
     
-//     if(appData.oneDayBudget < 100 && appData.oneDayBudget != 0){
-//         alert("Низкий уровень дохода");
-//     }
-//     else if (appData.oneDayBudget > 100 && appData.oneDayBudget < 1000){
-//         alert("Средний уровень дохода");
-//     }
-//     else if(appData.oneDayBudget > 1000){
-//         alert("Высокий уровень дохода");
-//     }
-//     else{
-//         alert('возможно не указан бюджет');
-//         askingBudget(money, date);
-//         appData.budget = money;
-//         detectDayBudget();
-//         detectLevel();
-//         return true;
-         
-//     }
+        if(appData.oneDayBudget < 100 && appData.oneDayBudget != 0){
+            alert("Низкий уровень дохода");
+        }
+        else if (appData.oneDayBudget > 100 && appData.oneDayBudget < 1000){
+            alert("Средний уровень дохода");
+        }
+        else if(appData.oneDayBudget > 1000){
+            alert("Высокий уровень дохода");
+        }
+        else{
+            alert('возможно не указан бюджет');
+            askingBudget(money, date);
+            appData.budget = money;
+            appData.detectDayBudget();
+            appData.detectLevel();
+            return true;
+        }
+    },
+    checkSavings:function (y){
+        if (appData.savings == true){
+            let save = +prompt("Какова сумма накоплений"),
+                percent = +prompt("Под какой процент");
     
-    
-// }
+            appData.monthIncome = save/100/12*percent;
+            alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+        }
+        y();
+    },
+    choseIncome: function(){
+        let extra = prompt("Что принесет дополнительный доход (Перечислите через запятую)", '');
+        if(extra == null || extra.length == 0){
+            alert("Нельзя осталять строку пустой или пропустить вопрос");
+            appData.choseIncome();
+        }
+        appData.income = extra.split(',');
+        appData.income.push(prompt('Может еще что-то забыли?',''));
+        appData.income.sort();
+        appData.income.forEach(function(item,i){
+            alert("Способы доп. заработка: " + (i+1) + " : " + item);
+        });
+    }
+};
+
+appData.choseExpenses();
+appData.chooseOptExpenses();
+appData.detectDayBudget();
+appData.checkSavings(appData.detectLevel);
+appData.choseIncome();
+
+function showAppDataObj(){
+    for (let items in appData) {
+        console.log("Наша программа включает в себя данные: " + items + " : " + appData[items]);
+        }
+    }
+
+showAppDataObj();
 
 
 
-// function checkSavings(y){
-//     if (appData.savings == true){
-//         let save = +prompt("Какова сумма накоплений"),
-//             percent = +prompt("Под какой процент");
 
-//         appData.monthIncome = save/100/12*percent;
-//         alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
-//     }
-//     y();
-// }
-// checkSavings(detectLevel);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -155,7 +198,7 @@
 //     );
 
 
-let arr = ["One", 2, "tree", 4, 5]; // создание массива
+// let arr = ["One", 2, "tree", 4, 5]; // создание массива
 // // arr.shift();// удаляет первый элемент массива 
 // // arr.unshift("20");// добавляетт элемент массива с  начала
 // // arr.pop();// убирает элемент массива с конца
@@ -164,18 +207,38 @@ let arr = ["One", 2, "tree", 4, 5]; // создание массива
 //     console.log(arr[i]);
 // } //перебор массива // второй способ с колбек функцией
 
-arr.forEach(function(item,i, mass){
- console.log(i + ":" + item + " " + mass);
-});
+// arr.forEach(function(item,i, mass){
+//  console.log(i + ":" + item + " " + mass);
+// });
+
+
+
+// let ans = "1,2,3,4,5,6,7,8,9"; // Метод Split прием данных в массив и определение разделителя
+// let arr = [];
+
+// arr = ans.split(",");
+// console.log(arr);
+
+
+
+// let arr = ["a","b","c","d", "e", "f"]; // удаление значений из массива начиная с позиции "2" следующие '"3" элемента
+
+// let removeItems = arr.splice(2,3);
+
+// console.log(arr);
+
+// console.log(removeItems);
 
 
 
 
 
+// for (let i = 0; i <= 100; ++i){ // Пример цикла с continue на остаток от деления. Continue прерывает итерацию и переходит к следующей
 
-
-
-
+//     // если true, пропустить оставшуюся часть тела цикла
+//     if (i % 2 == 0){continue;}
+//     console.log(i);
+// }
 
 
 
